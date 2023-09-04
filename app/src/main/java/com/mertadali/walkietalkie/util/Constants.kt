ab40864a55f9a2e2660b069cb24c14932d97cc1c
Mert Adalı <111153297+mertadali@users.noreply.github.com>
2023-09-04 16:51:57 +0300

    Update Constants.kt

--- app/src/main/java/com/mertadali/walkietalkie/util/Constants.kt
+++ app/src/main/java/com/mertadali/walkietalkie/util/Constants.kt
@@ -5,7 +5,7 @@ class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com/"
         const val CONTENT_TYPE = "application/json"
-        const val SERVER_KEY = "AAAAtvsjktQ:APA91bFB7q8Em3-GJWLMAgf_kYu5CQy46NsCDehq30AiORRwh6jdDE1dOmZFh5yy0xSpLOr4wO_vFqhCILlJi4hTGz5MhJCef4luTwUtohXsLz2fLexfFEX0y63GhWwRx7YNbFCYI421"
+        const val SERVER_KEY = ""
 
     }
 
